c70c0ea366add4db3ee7d1cdf72e8a4b306713a4
woxieao <xieao@huiber.cn>
2017-10-12 18:23:48 +0800

    add dalbase

--- XASoft.EfHelper/EfExtension.cs
+++ XASoft.EfHelper/EfExtension.cs
@@ -1,79 +1,79 @@
-﻿using System;
-using System.Linq;
-using System.Linq.Expressions;
-using XASoft.EfHelper.Models;
+﻿//using System;
+//using System.Linq;
+//using System.Linq.Expressions;
+//using XASoft.EfHelper.Models;
 
-namespace XASoft.EfHelper
-{
-    public static class EfExtension
-    {
-        internal class SwapVisitor : ExpressionVisitor
-        {
-            private readonly Expression _from, _to;
-            public SwapVisitor(Expression from, Expression to)
-            {
-                this._from = from;
-                this._to = to;
-            }
-            public override Expression Visit(Expression node)
-            {
-                return node == _from ? _to : base.Visit(node);
-            }
-        }
-        private static Expression<Func<TSource, bool>> CombineLambda<TSource>(this Expression<Func<TSource, bool>> predicate0, Expression<Func<TSource, bool>> predicate1)
-        {
-            return Expression.Lambda<Func<TSource, bool>>(Expression.AndAlso(
-          new SwapVisitor(predicate0.Parameters[0], predicate1.Parameters[0]).Visit(predicate0.Body),
-          predicate1.Body), predicate1.Parameters);
-        }
-        private static Expression<Func<TSource, bool>> DelDataFilter<TSource>(this Expression<Func<TSource, bool>> predicate) where TSource : DbBase
-        {
-            return predicate.CombineLambda(x => !x.DelFlag);
-        }
-        public static TSource FirstNotDel<TSource>(this IQueryable<TSource> source, Expression<Func<TSource, bool>> predicate) where TSource : DbBase
-        {
-            return source.First(predicate.DelDataFilter());
-        }
-        public static TSource FirstOrDefaultNotDel<TSource>(this IQueryable<TSource> source, Expression<Func<TSource, bool>> predicate) where TSource : DbBase
-        {
-            return source.FirstOrDefault(predicate.DelDataFilter());
+//namespace XASoft.EfHelper
+//{
+//    internal static class EfExtension
+//    {
+//        internal class SwapVisitor : ExpressionVisitor
+//        {
+//            private readonly Expression _from, _to;
+//            public SwapVisitor(Expression from, Expression to)
+//            {
+//                this._from = from;
+//                this._to = to;
+//            }
+//            public override Expression Visit(Expression node)
+//            {
+//                return node == _from ? _to : base.Visit(node);
+//            }
+//        }
+//        private static Expression<Func<TSource, bool>> CombineLambda<TSource>(this Expression<Func<TSource, bool>> predicate0, Expression<Func<TSource, bool>> predicate1)
+//        {
+//            return Expression.Lambda<Func<TSource, bool>>(Expression.AndAlso(
+//          new SwapVisitor(predicate0.Parameters[0], predicate1.Parameters[0]).Visit(predicate0.Body),
+//          predicate1.Body), predicate1.Parameters);
+//        }
+//        private static Expression<Func<TSource, bool>> DelDataFilter<TSource>(this Expression<Func<TSource, bool>> predicate) where TSource : DbBase
+//        {
+//            return predicate.CombineLambda(x => !x.DelFlag);
+//        }
+//        public static TSource FirstNotDel<TSource>(this IQueryable<TSource> source, Expression<Func<TSource, bool>> predicate) where TSource : DbBase
+//        {
+//            return source.First(predicate.DelDataFilter());
+//        }
+//        public static TSource FirstOrDefaultNotDel<TSource>(this IQueryable<TSource> source, Expression<Func<TSource, bool>> predicate) where TSource : DbBase
+//        {
+//            return source.FirstOrDefault(predicate.DelDataFilter());
 
-        }
-        public static IQueryable<TSource> WhereNotDel<TSource>(this IQueryable<TSource> source, Expression<Func<TSource, bool>> predicate) where TSource : DbBase
-        {
-            return source.Where(predicate.DelDataFilter());
-        }
-        public static TSource SingleOrDefaultNotDel<TSource>(this IQueryable<TSource> source, Expression<Func<TSource, bool>> predicate) where TSource : DbBase
-        {
-            return source.SingleOrDefault(predicate.DelDataFilter());
-        }
-        public static TSource SingleNotDel<TSource>(this IQueryable<TSource> source, Expression<Func<TSource, bool>> predicate) where TSource : DbBase
-        {
-            return source.Single(predicate.DelDataFilter());
-        }
-        public static PagingData<TSource> GetList<TSource>(this IQueryable<TSource> source, Expression<Func<TSource, bool>> predicate, int pageIndex, int pageSize) where TSource : DbBase
-        {
-            var paging = new Paging(pageIndex, pageSize);
-            var p = predicate.DelDataFilter();
-            return new PagingData<TSource>
-            {
-                List = source.Where(p).OrderByDescending(i => i.Id).Skip(paging.Skip).Take(paging.PageSize),
-                TotalCount = source.Count(p)
-            };
-        }
-        public static PagingData<TSource> GetList<TSource>(this IQueryable<TSource> source, int pageIndex, int pageSize) where TSource : DbBase
-        {
-            var paging = new Paging(pageIndex, pageSize);
-            return new PagingData<TSource>
-            {
-                List = source.OrderByDescending(i => i.Id).Skip(paging.Skip).Take(paging.PageSize),
-                TotalCount = source.Count()
-            };
-        }
-        public static void Delete<TSource>(TSource entity) where TSource : DbBase
-        {
-            entity.DelFlag = true;
-        }
-    }
-}
+//        }
+//        public static IQueryable<TSource> WhereNotDel<TSource>(this IQueryable<TSource> source, Expression<Func<TSource, bool>> predicate) where TSource : DbBase
+//        {
+//            return source.Where(predicate.DelDataFilter());
+//        }
+//        public static TSource SingleOrDefaultNotDel<TSource>(this IQueryable<TSource> source, Expression<Func<TSource, bool>> predicate) where TSource : DbBase
+//        {
+//            return source.SingleOrDefault(predicate.DelDataFilter());
+//        }
+//        public static TSource SingleNotDel<TSource>(this IQueryable<TSource> source, Expression<Func<TSource, bool>> predicate) where TSource : DbBase
+//        {
+//            return source.Single(predicate.DelDataFilter());
+//        }
+//        public static PagingData<TSource> GetList<TSource>(this IQueryable<TSource> source, Expression<Func<TSource, bool>> predicate, int pageIndex, int pageSize) where TSource : DbBase
+//        {
+//            var paging = new Paging(pageIndex, pageSize);
+//            var p = predicate.DelDataFilter();
+//            return new PagingData<TSource>
+//            {
+//                List = source.Where(p).OrderByDescending(i => i.Id).Skip(paging.Skip).Take(paging.PageSize),
+//                TotalCount = source.Count(p)
+//            };
+//        }
+//        public static PagingData<TSource> GetList<TSource>(this IQueryable<TSource> source, int pageIndex, int pageSize) where TSource : DbBase
+//        {
+//            var paging = new Paging(pageIndex, pageSize);
+//            return new PagingData<TSource>
+//            {
+//                List = source.OrderByDescending(i => i.Id).Skip(paging.Skip).Take(paging.PageSize),
+//                TotalCount = source.Count()
+//            };
+//        }
+//        public static void Delete<TSource>(TSource entity) where TSource : DbBase
+//        {
+//            entity.DelFlag = true;
+//        }
+//    }
+//}
 
---
+++ XASoft.EfHelper/Models/DalBase.cs
@@ -0,0 +1,91 @@
+﻿using System;
+using System.Data.Entity;
+using System.Linq;
+using System.Linq.Expressions;
+
+namespace XASoft.EfHelper.Models
+{
+
+    public abstract class DalBase<TSource, TEntity>
+    where TSource : DbBase
+        where TEntity : DbContext
+    {
+        protected readonly IQueryable<TSource> Source;
+        protected readonly TEntity Db;
+        protected DalBase(TEntity db)
+        {
+            Db = db;
+            Source = db.Set<TSource>();
+        }
+
+
+        internal class SwapVisitor : ExpressionVisitor
+        {
+            private readonly Expression _from, _to;
+            public SwapVisitor(Expression from, Expression to)
+            {
+                this._from = from;
+                this._to = to;
+            }
+            public override Expression Visit(Expression node)
+            {
+                return node == _from ? _to : base.Visit(node);
+            }
+        }
+        private Expression<Func<TSource, bool>> CombineLambda(Expression<Func<TSource, bool>> predicate0, Expression<Func<TSource, bool>> predicate1)
+        {
+            return Expression.Lambda<Func<TSource, bool>>(Expression.AndAlso(
+          new SwapVisitor(predicate0.Parameters[0], predicate1.Parameters[0]).Visit(predicate0.Body),
+          predicate1.Body), predicate1.Parameters);
+        }
+        private Expression<Func<TSource, bool>> DelDataFilter(Expression<Func<TSource, bool>> predicate)
+        {
+            return CombineLambda(predicate, x => !x.DelFlag);
+        }
+        public TSource FirstNotDel(Expression<Func<TSource, bool>> predicate)
+        {
+            return Source.First(DelDataFilter(predicate));
+        }
+        public TSource FirstOrDefaultNotDel(Expression<Func<TSource, bool>> predicate)
+        {
+            return Source.FirstOrDefault(DelDataFilter(predicate));
+
+        }
+        public IQueryable<TSource> WhereNotDel(Expression<Func<TSource, bool>> predicate)
+        {
+            return Source.Where(DelDataFilter(predicate));
+        }
+        public TSource SingleOrDefaultNotDel(Expression<Func<TSource, bool>> predicate)
+        {
+            return Source.SingleOrDefault(DelDataFilter(predicate));
+        }
+        public TSource SingleNotDel(Expression<Func<TSource, bool>> predicate)
+        {
+            return Source.Single(DelDataFilter(predicate));
+        }
+        public PagingData<TSource> GetList(Expression<Func<TSource, bool>> predicate, int pageIndex, int pageSize)
+        {
+            var paging = new Paging(pageIndex, pageSize);
+            var p = DelDataFilter(predicate);
+            return new PagingData<TSource>
+            {
+                List = Source.Where(p).OrderByDescending(i => i.Id).Skip(paging.Skip).Take(paging.PageSize),
+                TotalCount = Source.Count(p)
+            };
+        }
+        public PagingData<TSource> GetList(int pageIndex, int pageSize)
+        {
+            var paging = new Paging(pageIndex, pageSize);
+            return new PagingData<TSource>
+            {
+                List = Source.Where(i => !i.DelFlag).OrderByDescending(i => i.Id).Skip(paging.Skip).Take(paging.PageSize),
+                TotalCount = Source.Count(i => !i.DelFlag)
+            };
+        }
+        public void Delete(TSource entity)
+        {
+            entity.DelFlag = true;
+        }
+    }
+
+}
