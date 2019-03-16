5fa2a0880d9cbda9e2f0a55bce77b21ea68eb83b
woxieao <867993946@qq.comhexo scd e:/git config --global user.email 867993946@qq.com>
2019-03-17 02:13:27 +0800

    rename

--- XASoft.Extensions/MergerExtensions.cs
+++ XASoft.Extensions/MergerExtensions.cs
@@ -6,7 +6,7 @@ using XASoft.CommonModel.PagingModel;
 
 namespace XASoft.Extensions
 {
-    public static class DynamicExtensions
+    public static class MergerExtensions
     {
         private static void Copy2Dynamic<T>(T sourceData, IDictionary<string, object> resultRef) where T : class
         {
@@ -36,7 +36,7 @@ namespace XASoft.Extensions
         /// <typeparam name="T"></typeparam>
         /// <param name="sourceData">sourceData</param>
         /// <returns>new dynamic object not reference sourceData</returns>
-        public static dynamic ToDynamic<T>(this T sourceData) where T : class
+        public static dynamic Merger<T>(this T sourceData) where T : class
         {
             IDictionary<string, object> result = new ExpandoObject();
             Copy2Dynamic(sourceData, result);
@@ -51,7 +51,7 @@ namespace XASoft.Extensions
         /// <param name="sourceData">sourceData</param>
         /// <param name="combineVal">combineValue</param>
         /// <returns>new dynamic object not reference sourceData</returns>
-        public static dynamic ToDynamic<T0, T1>(this T0 sourceData, T1 combineVal)
+        public static dynamic Merger<T0, T1>(this T0 sourceData, T1 combineVal)
             where T0 : class
             where T1 : class
         {
@@ -61,7 +61,7 @@ namespace XASoft.Extensions
             return result;
         }
 
-        public static dynamic ToDynamic<T0, T1>(this T0 sourceData, Func<T0, T1> combineValFunc)
+        public static dynamic Merger<T0, T1>(this T0 sourceData, Func<T0, T1> combineValFunc)
             where T0 : class
             where T1 : class
         {
@@ -71,7 +71,7 @@ namespace XASoft.Extensions
             return result;
         }
 
-        public static dynamic ToDynamic<T0, T1, T2>(this T0 sourceData, T1 combineVal0, T2 combineVal1)
+        public static dynamic Merger<T0, T1, T2>(this T0 sourceData, T1 combineVal0, T2 combineVal1)
             where T0 : class
             where T1 : class
             where T2 : class
@@ -84,23 +84,23 @@ namespace XASoft.Extensions
         }
 
 
-        public static IEnumerable<dynamic> ToDynamicList<T0, T1>(this IEnumerable<T0> sourceDataList, Func<T0, T1> func)
+        public static IEnumerable<dynamic> ListMerger<T0, T1>(this IEnumerable<T0> sourceDataList, Func<T0, T1> func)
             where T0 : class
             where T1 : class
         {
             var resultList = new List<IDictionary<string, object>>();
             foreach (var sourceData in sourceDataList)
             {
-                resultList.Add((IDictionary<string, object>)sourceData.ToDynamic(func));
+                resultList.Add((IDictionary<string, object>)sourceData.Merger(func));
             }
             return resultList;
         }
 
-        public static PagingData<dynamic> ToDynamicPagingData<T0, T1>(this PagingData<T0> sourceDataList, Func<T0, T1> func)
+        public static PagingData<dynamic> PagingDataMerger<T0, T1>(this PagingData<T0> sourceDataList, Func<T0, T1> func)
             where T0 : class
             where T1 : class
         {
-            var resultList = sourceDataList.List.ToDynamicList(func);
+            var resultList = sourceDataList.List.ListMerger(func);
             return new PagingData<dynamic>
             {
                 PageCount = sourceDataList.PageCount,
